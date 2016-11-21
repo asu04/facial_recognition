0d9c70e4f53f5110a59e7ad860c88c89e4a33139
Alex Su <alexsu04@gmail.com>
2016-11-21 17:25:12 -0600

    Fixed silly mistake

--- code/facial_recognition.py
+++ code/facial_recognition.py
@@ -20,32 +20,32 @@ faceNet = ann.neuralNet(2, layer_list)
 #Backpropagation
 
 def backpropagate(training_data, rate, momentum):
-    # Work out all the output deltas first
-    final_delta = np.zeros(faceNet.layers[1].weight_matrix.shape[1])
+    total_weight_1 = np.zeros(faceNet.layers[1].weight_matrix.shape)
+    total_weight_2 = np.zeros(faceNet.layers[0].weight_matrix.shape)
     for data in training_data:
         target = data[1]
         features = data[0]
-        final_output = faceNet.output(features)
+        final_output = faceNet.propagate(features)
+        print final_output
         error = target - final_output
         delta = error*(final_output)*(1-final_output)
-        final_delta += delta
-    # Now backpropgate all the output deltas to find the rest of the deltas
-    hidden_deltas = faceNet.layers[1].emit_deltas(final_delta)
-    # Now compute all necessary weight updates and store back in weight
-    # matrix
-    weight_1 = np.tile(faceNet.layers[1].last_input, (faceNet.layers[1].weight_matrix.shape[1],1)).T
-    weight_2 = np.tile(faceNet.layers[0].last_input, (faceNet.layers[0].weight_matrix.shape[1],1)).T
+        # Now backpropgate all the output deltas to find the rest of the deltas
+        hidden_deltas = faceNet.layers[1].emit_deltas(final_delta)
+        # Now compute all necessary weight updates and store back in weight
+        # matrix
+        weight_1 = np.tile(faceNet.layers[1].last_input, (faceNet.layers[1].weight_matrix.shape[1],1)).T
+        weight_2 = np.tile(faceNet.layers[0].last_input, (faceNet.layers[0].weight_matrix.shape[1],1)).T
+        weight_1 *= (final_delta * rate)
+        weight_2 *= (hidden_deltas * rate)
+        total_weight_1 += weight_1
+        total_weight_2 += weight_2
 
-    weight_1 *= (final_delta * rate)
-    weight_1 += (momentum * faceNet.layers[1].last_weight_update)
-    faceNet.layers[1].last_weight_update = np.copy(weight_1)
-
-    weight_2 *= (hidden_deltas * rate)
-    weight_2 += (momentum * faceNet.layers[0].last_weight_update)
-    faceNet.layers[0].laste_weight_update = np.copy(weight_2)
-
-    faceNet.layers[0].weight_matrix += weight_2
-    faceNet.layers[1].weight_matrix += weight_1
+    total_weight_1 += (momentum * faceNet.layers[1].last_weight_update)
+    total_weight_2 += (momentum * faceNet.layers[0].last_weight_update)
+    faceNet.layers[0].last_weight_update = np.copy(total_weight_2)
+    faceNet.layers[1].last_weight_update = np.copy(total_weight_1)
+    faceNet.layers[0].weight_matrix += total_weight_2
+    faceNet.layers[1].weight_matrix += total_weight_1
 
 # def backpropagate(training_data, rate, momentum, current_layer):
     # final_delta = np.zeros(faceNet.layers[current_layer].shape[1])
