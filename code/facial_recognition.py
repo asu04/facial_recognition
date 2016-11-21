abd28dce6b49c11654764003a5435d2445f19b1a
Alex Su <alexsu04@gmail.com>
2016-11-21 16:59:03 -0600

    Rough code done

--- code/facial_recognition.py
+++ code/facial_recognition.py
@@ -1,5 +1,6 @@
 import numpy as np
 import neural_network as ann
+from scipy.misc import imread
 
 DATA_PATH = "/home/asu/Projects/facial_recognition/code"
 RANDOM_HIGH = 0.5
@@ -7,31 +8,55 @@ RANDOM_LOW = -0.5
 
 #Initialize weight matrices with small random weights by layer
 
-layer1 = np.zeros((960,3))
-layer2 = (RANDOM_HIGH - RANDOM_LOW) * np.random.rand(4,4) + RANDOM_LOW
+layer1 = ann.neuralLayer(np.zeros((960,3)))
+layer2 = ann.neuralLayer(((RANDOM_HIGH - RANDOM_LOW) * np.random.rand(4,4) + RANDOM_LOW),thresholded=True)
 
-matrix_list = [layer1, layer2]
+layer_list = [layer1, layer2]
 
 #Initialize network
 
-faceNet = ann.neuralNet(2, matrix_list)
+faceNet = ann.neuralNet(2, layer_list)
 
 #Backpropagation
-#Testcomment
 
-def backpropagate(training_data, rate, momentum, current_layer):
-    final_delta = np.zeros(faceNet.layers[current_layer].shape[1])
+def backpropagate(training_data, rate, momentum):
+    # Work out all the output deltas first
+    final_delta = np.zeros(faceNet.layers[1].weight_matrix.shape[1])
     for data in training_data:
         target = data[1]
         features = data[0]
-        # Output units are thresholded, so need to add a 1 in the output of the
-        # hidden layer here
-        hidden_output = np.insert(faceNet.propagate(features, forward_one=True),0, 1.0)
-        final_output = faceNet.propagate(hidden_output, current_layer = 1)
+        final_output = faceNet.output(features)
         error = target - final_output
         delta = error*(final_output)*(1-final_output)
         final_delta += delta
-    for i in range(faceNet.layers[current_layer].shape[0]):
-        faceNet.layers[current_layer].T[i] += rate * hidden_output * final_delta
+    # Now backpropgate all the output deltas to find the rest of the deltas
+    hidden_deltas = faceNet.layers[1].emit_deltas(final_delta)
+    # Now compute all necessary weight updates and store back in weight
+    # matrix
+    weight_1 = np.tile(faceNet.layers[1].last_input, (faceNet.layers[1].weight_matrix.shape[1],1)).T
+    weight_2 = np.tile(faceNet.layers[0].last_input, (faceNet.layers[0].weight_matrix.shape[1],1)).T
+
+    weight_1 *= (final_delta * rate)
+    weight_1 += (momentum * faceNet.layers[1].last_weight_update)
+    faceNet.layers[1].last_weight_update = np.copy(weight_1)
+
+    weight_2 *= (hidden_deltas * rate)
+    weight_2 += (momentum * faceNet.layers[0].last_weight_update)
+    faceNet.layers[0].laste_weight_update = np.copy(weight_2)
+
+    faceNet.layers[0].weight_matrix += weight_2
+    faceNet.layers[1].weight_matrix += weight_1
+
+# def backpropagate(training_data, rate, momentum, current_layer):
+    # final_delta = np.zeros(faceNet.layers[current_layer].shape[1])
+    # for data in training_data:
+        # target = data[1]
+        # features = data[0]
+        # final_output = faceNet.output(features)
+        # error = target - final_output
+        # delta = error*(final_output)*(1-final_output)
+        # final_delta += delta
+    # for i in range(faceNet.layers[current_layer].shape[0]):
+        # faceNet.layers[current_layer].T[i] += rate * hidden_output * final_delta
 
 
--- code/neural_network.py
+++ code/neural_network.py
@@ -8,7 +8,6 @@ def sigmoid(x):
 
 class neuralNode:
     """Base class for node in neural network"""
-
     def __init__(self, weights):
         if isinstance(weights, np.ndarray):
             if len(weights.shape) == 1:
@@ -19,28 +18,56 @@ class neuralNode:
     def output(self, inputs):
         return sigmoid((inputs * self.weights).sum())
 
+class neuralLayer:
+    """Class for layer in network"""
+    def __init__(self, weight_matrix, thresholded = False):
+        try:
+            self.thresholded = thresholded
+            self.weight_matrix = weight_matrix
+            self.last_weight_update = np.zeros(weight_matrix.shape)
+        except:
+            raise ValueError("Unexpected arguments")
+
+    def output(self, inputs):
+        output_values = []
+        if self.thresholded:
+            inputs = np.insert(inputs,0,1)
+        for row in self.weight_matrix.T:
+            output_values.append(neuralNode(row).output(inputs))
+        output_vector = np.array(output_values)
+        # Keep track of last output and input
+        self.last_output = np.copy(output_vector)
+        self.last_input = np.copy(inputs)
+        return output_vector
+
+    def emit_deltas(self, next_deltas):
+        deltas = np.multiply(self.weight_matrix, next_deltas)
+        if self.thresholded:
+            deltas = np.delete(deltas,0,0)
+        return deltas.sum(axis = 1)
 
 class neuralNet:
     """Base class for neural net"""
 
-    def __init__(self, n_layers, matrix_list):
-        if not len(matrix_list) == n_layers:
+    def __init__(self, n_layers, layer_list):
+        if not len(layer_list) == n_layers:
             raise ValueError('Number of layers and number of matrices supplied do not match')
         try:
             self.n_layers = n_layers
             self.layers = []
-            for layer in matrix_list:
+            for layer in layer_list:
                 self.layers.append(layer)
-
         except:
             raise ValueError('DEBUG_MESSAGE_HERE')
 
-    def propagate(self, inputs, current_layer = 0, forward_one = False):
-        output_values = []
-        for row in self.layers[current_layer].T:
-            output_values.append(neuralNode(row).output(inputs))
-        output_vector = np.array(output_values)
-        if current_layer == self.n_layers - 1 or forward_one:
-            return output_vector
-        else:
-            return self.propagate(output_vector, current_layer + 1)
+    def propagate(self, inputs):
+        for layer in self.layers:
+            inputs = layer.output(inputs)
+        return inputs
+#         for row in self.layers[current_layer].T:
+            # output_values.append(neuralNode(row).output(inputs))
+        # output_vector = np.array(output_values)
+        # if current_layer == self.n_layers - 1 or forward_one:
+            # return output_vector
+        # else:
+#             return self.propagate(output_vector, current_layer + 1)
